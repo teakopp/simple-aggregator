740d6874c9b8a5e6bcfd48ec61a9732727d6c942
teakopp <teakopp@protonmail.com>
2023-02-24 11:47:31 -0800

    fix tests to match date/time formatting

--- tests/test_aggregator.py
+++ tests/test_aggregator.py
@@ -3,21 +3,21 @@ from simple_aggregator.modules.aggregator import Aggregator
 def test_correct_report():
     a = Aggregator()
     a.add_event("2021-03-01 20:01")
-    assert a.report() == {'2021-03-01-20': 1} 
+    assert a.report() == {'2021-03-01T20:00:00Z': 1} 
 
 def test_single_day():
     a = Aggregator()
     a.add_event("2021-03-01 20:31")
     a.add_event("2021-03-01 20:41")
     a.add_event("2021-03-01 20:51")
-    assert a.report() == {'2021-03-01-20': 3} 
+    assert a.report() == {'2021-03-01T20:00:00Z': 3} 
 
 def test_multiple_hours():
     a = Aggregator()
     a.add_event("2021-03-01 20:31")
     a.add_event("2021-03-01 21:41")
     a.add_event("2021-03-01 22:51")
-    assert a.report() == {'2021-03-01-20': 1, '2021-03-01-21': 1, '2021-03-01-22': 1} 
+    assert a.report() == {'2021-03-01T20:00:00Z': 1, '2021-03-01T21:00:00Z': 1, '2021-03-01T22:00:00Z': 1} 
 
 def test_correct_report_multiple_day():
     a = Aggregator()
@@ -25,4 +25,4 @@ def test_correct_report_multiple_day():
     a.add_event("2021-04-11 20:41")
     a.add_event("2021-05-23 20:55")
     a.add_event("2021-05-23 20:51")
-    assert a.report() == {'2021-03-21-20': 1, '2021-04-11-20': 1, '2021-05-23-20' : 2} 
+    assert a.report() == {'2021-03-21T20:00:00Z': 1, '2021-04-11T20:00:00Z': 1, '2021-05-23T20:00:00Z' : 2} 
